55d13f1e50dd0ee7f0ec97e79a2937b981f6dea3
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2016-10-21 23:57:14 -0700

    I added more info on the reference stars to the tables. Also, explained more about the dichroic and MORIS imager band choice.

--- make_tables.py
+++ make_tables.py
@@ -1,6 +1,16 @@
 from astropy.io import ascii
+import subprocess
+import pdb
 
 for oneTable in ['bd_table','observing_table']:
+    ## Cleanup CSV file for what MS Excel does
+    subprocess.call(['mac2unix',oneTable+'.csv'])
     dat = ascii.read(oneTable+'.csv')
-    dat.write(oneTable+'.tex')
+    ascii.write(dat,oneTable+'.tex',
+                Writer=ascii.Latex,latexdict={'tabletype':'table*'})
+    #dat.write(oneTable+'.tex',latexdict={'tabletype':'table*'},Writer=ascii.latex)
     
+    with open(oneTable+'.tex') as TableTex:
+        allLines = TableTex.readlines()
+        ## Add a horizontal line under headings
+        #pdb.set_trace()
